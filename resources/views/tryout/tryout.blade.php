f90ce4e55e182a6451671059c19d524f763709d7
Rizal <rizal2726f@gmail.com>
2025-05-20 17:48:45 +0700

    center modal

--- resources/views/tryout/tryout.blade.php
+++ resources/views/tryout/tryout.blade.php
@@ -83,7 +83,7 @@
 
     <!-- Modal: Konfirmasi Selesai -->
     <div class="modal fade" id="selesaiModal" tabindex="-1" aria-labelledby="selesaiModalLabel" aria-hidden="true">
-        <div class="modal-dialog">
+        <div class="modal-dialog modal-dialog-centered">
             <div class="modal-content">
                 <div class="modal-header">
                     <h5 class="modal-title">Konfirmasi Selesai</h5>
@@ -102,7 +102,7 @@
 
     <!-- Modal: Lapor Soal -->
     <div class="modal fade" id="modalId" tabindex="-1">
-        <div class="modal-dialog">
+        <div class="modal-dialog modal-dialog-centered">
             <div class="modal-content">
                 <div class="modal-header">
                     <h5 class="modal-title">Laporkan Soal</h5>
